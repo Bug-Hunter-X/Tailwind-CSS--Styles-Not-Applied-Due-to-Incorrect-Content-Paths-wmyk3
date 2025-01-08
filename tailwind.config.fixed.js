7d1abcca7247abb6bef21f3e64e8533ffc6a9124
BugHunter-X <askzenai@gmail.com>
2025-01-08 14:28:05 +0100

    Add tailwind.config.fixed.js file

---
+++ tailwind.config.fixed.js
@@ -0,0 +1,7 @@
+```javascript
+// ... your Tailwind CSS configuration
+module.exports = {
+  // ... other configuration options
+  content: ['./src/**/*.{html,js,ts,jsx,tsx,vue}'], // Corrected path
+};
+```

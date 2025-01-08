d211bf667813159a171be6fdbbff0bbfcac9fe8b
BugHunter-X <askzenai@gmail.com>
2025-01-08 14:28:05 +0100

    Add tailwind.config.js file

---
+++ tailwind.config.js
@@ -0,0 +1,7 @@
+```javascript
+// ... your Tailwind CSS configuration
+module.exports = {
+  // ... other configuration options
+  content: ['./src/**/*.{html,js,ts,jsx,tsx}'], // This line might be wrong
+};
+```
